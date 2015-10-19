e4616618633573099145a69c0a8c7332f7e7c2b1
unknown <1310746@RG43008.rgu.ac.uk>
2015-10-19 14:50:33 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPEhtml>
+<html>
+<head lang="en">
+    <meta charset="UTF-8"
+    <TITLE>Deployment Test</TITLE>
+</head>
+<body>
+<H1> it's ALIVE</H1>
+</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: 1310746
+ * Date: 19/10/2015
+ * Time: 14:28
+ */
+class index
+{
+
+}
