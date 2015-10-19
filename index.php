27248d55f61ed8a00c4e86b46d5515c2ba3515d9
1310746 <sult8@hotmail.com>
2015-10-19 16:48:58 +0100

    test 1

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8"
-    <TITLE>Deployment Test</TITLE>
 </head>
 <body>
 <h1>Page Title</h1>
@@ -12,19 +11,14 @@
     <li>List item 2</li>
 </ul>
 
+<?
+ echo 3 * 4;
+
+?>
+
 
 
 
 
 </body>
 </html>
-/**
- * Created by PhpStorm.
- * User: 1310746
- * Date: 19/10/2015
- * Time: 14:28
- */
-class index
-{
-
-}
